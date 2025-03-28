f149fa9445995e9bd0426cf7a5578b032fda0599
Roy Kathurima <kathurimaroy@gmail.com>
2025-03-28 10:27:00 +0300

    Added a result type to the service methods

--- app/src/main/java/org/example/TemperatureService.java
+++ app/src/main/java/org/example/TemperatureService.java
@@ -4,16 +4,19 @@ package org.example;
 import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
+import jakarta.jws.WebResult;
 
 @WebService
 public class TemperatureService {
 
     @WebMethod
+    @WebResult(name = "temperatureInFarenheit")
     public double celsiusToFarenheit(@WebParam(name = "celsius") double celsius) {
         return (celsius * 9 / 5) + 32;
     }
 
     @WebMethod
+    @WebResult(name = "temperatureInFarenheit")
     public double farenheitToCelsius(@WebParam(name = "farenheit") double farenheit) {
         return (farenheit - 32) * 5 / 9;
     }
